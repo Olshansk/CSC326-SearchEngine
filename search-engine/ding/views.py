3681c3bfe166a61b2099760e5db13c53e607f75a
Amandeep Grewal <me@amandeep.ca>
2013-10-27 22:30:34 -0400

    Fixed frontend formatting and grammar/spelling issues.

--- search-engine/ding/views.py
+++ search-engine/ding/views.py
@@ -2,13 +2,17 @@ from collections import Counter
 from collections import OrderedDict
 import operator
 import urllib
+
 from django.core import serializers
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from ding.models import SearchWord, Document, Word
 from django.db.models.query import EmptyQuerySet
 
+from ding.models import SearchWord, Word
+
+
 RESULTS_PER_PAGE = 20
+
 
 # Does setup work and renders the search page
 def search(request):
@@ -25,11 +29,13 @@ def search(request):
     context = {'frequency_dictionary': words.iteritems()}
     return render(request, 'ding/search.html', context)
 
+
 # This view is used to redirect the post request from the search form
 def parsed_query_redirect(request):
     query = request.POST['query']
     url = "http://localhost:8000/ding/parsed_query?query=" + query
     return redirect(url)
+
 
 # Does setup work and renders the results page
 def parsed_query(request):
@@ -65,13 +71,15 @@ def parsed_query(request):
 
     return render(request, 'ding/parsed_query.html', context)
 
+
 # Used to make agex calls to retrieve more search results
 def get_search_results(request, query, scroll_num):
     # Retrieve list of search results
     filtered_docs = document_objects_for_keyword_in_range(int(scroll_num), query)
     # Serializes results into json
-    jsonData = serializers.serialize('json', filtered_docs, fields=('title','url', 'description'))
-    return HttpResponse(jsonData, mimetype="application/json")
+    json_data = serializers.serialize('json', filtered_docs, fields=('title', 'url', 'description'))
+    return HttpResponse(json_data, mimetype="application/json")
+
 
 def document_objects_for_keyword_in_range(first_index, keyword):
     #unescapes and splits the query into keywords
@@ -82,29 +90,35 @@ def document_objects_for_keyword_in_range(first_index, keyword):
         word = get_or_none(Word, text=key)
         if word is not None:
             query_set = query_set | word.document_set.all()
-    # Orders the set, removes duplicates, and returns a list of the resultant documents
-    documentObejcts = list(query_set.order_by("-pagerank").distinct()[first_index * RESULTS_PER_PAGE: (first_index + 1) * RESULTS_PER_PAGE])
-    return documentObejcts
+        # Orders the set, removes duplicates, and returns a list of the resultant documents
+    document_objects = list(query_set.order_by("-pagerank").distinct()[
+                            first_index * RESULTS_PER_PAGE: (first_index + 1) * RESULTS_PER_PAGE])
+    return document_objects
 
-# Returns a None object if there is not Word obejct corresponding to the keyword
+
+# Returns a None object if there is not Word object corresponding to the keyword
 def get_or_none(model, **kwargs):
     try:
         return model.objects.get(**kwargs)
     except model.DoesNotExist:
         return None
 
+
 def error_400(request):
     error = {'error_type': 400, 'request_path': request.path}
     return render(request, 'ding/error.html', error, status=400)
+
 
 def error_403(request):
     error = {'error_type': 403, 'request_path': request.path}
     return render(request, 'ding/error.html', error, status=403)
 
+
 def error_404(request):
     error = {'error_type': 404, 'request_path': request.path}
     return render(request, 'ding/error.html', error, status=404)
 
+
 def error_500(request):
     error = {'error_type': 500, 'request_path': request.path}
-    return render(request, 'ding/error.html', error , status=500)
+    return render(request, 'ding/error.html', error, status=500)
