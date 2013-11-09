840f44f9ed2f9a34f654862704121f5d3749091e
Amandeep Grewal <me@amandeep.ca>
2013-11-09 07:22:13 +0000

    Change some settings to aid in EC2 deployment

--- search-engine/search-engine/settings.py
+++ search-engine/search-engine/settings.py
@@ -3,7 +3,7 @@ import os
 
 DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = False
+DEBUG = os.environ.get('DING_DEBUG') or False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite3', # Or path to database file if using sqlite3.
+        'NAME': os.environ.get('DING_SQLITE_NAME') or 'db.sqlite3', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -26,7 +26,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
