03733c4b75f8830d1b20ce666a5b22fa34895524
Amandeep Grewal <me@amandeep.ca>
2013-11-10 05:38:29 +0000

    Add comments to install script.

--- install_ec2.sh
+++ install_ec2.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# Install Apache, WSGI, and PIP
 sudo apt-get install apache2 libapache2-mod-wsgi python-pip -y
 
+# Install Ding python dependencies
 sudo pip install -r requirements.txt
 
+# Create Apache configuration
 sudo cp -T ding.conf /etc/apache2/sites-available/ding.conf
 sudo a2ensite ding
 
+# Create db and crawl
 cd search-engine
 python manage.py syncdb --noinput
 python manage.py crawl
 
+# Set permissions on db so ubuntu user and Apache can access it
 cd ..
 sudo chown www-data search-engine/db.sqlite3
 sudo chown www-data search-engine
@@ -18,5 +23,6 @@ sudo chmod 770 search-engine/db.sqlite3
 sudo chmod 770 search-engine
 sudo usermod -a -G www-data ubuntu
 
+# Restart Apache server
 sudo /etc/init.d/apache2 restart
 
