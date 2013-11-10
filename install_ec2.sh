79a5e7e089426fc9d7ea525ced628558a11d850f
Amandeep Grewal <me@amandeep.ca>
2013-11-10 05:31:01 +0000

    Fix permissions in install script. Remove confirmations in apt-get and syncdb.

--- install_ec2.sh
+++ install_ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install apache2 libapache2-mod-wsgi python-pip
+sudo apt-get install apache2 libapache2-mod-wsgi python-pip -y
 
 sudo pip install -r requirements.txt
 
@@ -8,10 +8,15 @@ sudo cp -T ding.conf /etc/apache2/sites-available/ding.conf
 sudo a2ensite ding
 
 cd search-engine
-python manage.py syncdb
-#python manage.py crawl
+python manage.py syncdb --noinput
+python manage.py crawl
 
-sudo chown www-data db.sqlite3
+cd ..
+sudo chown www-data search-engine/db.sqlite3
+sudo chown www-data search-engine
+sudo chmod 770 search-engine/db.sqlite3
+sudo chmod 770 search-engine
+sudo usermod -a -G www-data ubuntu
 
 sudo /etc/init.d/apache2 restart
 
